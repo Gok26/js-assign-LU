70f26865479cedaf363787421089fa5c67c21817
Gok26 <68271091+Gok26@users.noreply.github.com>
2020-07-27 08:57:03 +0530

    Add files via upload

---
+++ assign-4/arr.js
@@ -0,0 +1,5 @@
+let shoppingList =['Groceries','Perfume','Shoes','Shades','Wristwatch']
+let shoppingBasket = [...shoppingList]; // copying the elements of an array
+console.log(shoppingBasket);
+shoppingBasket.push('Wallet'); // adding new element
+console.log(shoppingBasket);
